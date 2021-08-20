832e8115086dbd610763daf53255260d5b70e357
FacundoEG <F_E_G.93@hotmail.com>
2021-08-20 00:13:32 -0300

    modifique el router para subir el desafio a git

--- src/router.ts
+++ src/router.ts
@@ -5,55 +5,63 @@ import { initWinPage } from "./pages/win";
 import { initDefeatPage } from "./pages/defeat";
 
 const rutas = [
- {
-  path: /\/welcome/ ,
-  component: initWelcomePage
- },
- {
-  path: /\/index/ ,
-  component: initIndexPage
- },
- {
-  path: /\/game/ ,
-  component: initGamePage
- },
- {
-  path: /\/victoria/ ,
-  component: initWinPage
- },
- {
-  path: /\/derrota/ ,
-  component: initDefeatPage
- }
-]
+  {
+    path: /\/welcome/,
+    component: initWelcomePage,
+  },
+  {
+    path: /\/desafio-m5/,
+    component: initWelcomePage,
+  },
+  {
+    path: /\/index/,
+    component: initIndexPage,
+  },
+  {
+    path: /\/game/,
+    component: initGamePage,
+  },
+  {
+    path: /\/victoria/,
+    component: initWinPage,
+  },
+  {
+    path: /\/derrota/,
+    component: initDefeatPage,
+  },
+];
 
-export function initRouter(container:Element){
+export function initRouter(container: Element) {
   function goTo(path) {
     history.pushState({}, "", path);
     handleRoute(path);
   }
 
   function handleRoute(route) {
-    container.innerHTML = ""
-  
-   for (const r of rutas) {
-     if(r.path.test(route)) {
-       const el = r.component({goTo:goTo})
-       container.appendChild(el)
-     }
-   }
+    container.innerHTML = "";
+
+    for (const r of rutas) {
+      if (r.path.test(route)) {
+        const el = r.component({ goTo: goTo });
+        container.appendChild(el);
+      }
+    }
   }
- 
+
   if (location.pathname == "/") {
     goTo("/welcome");
   } else {
     handleRoute(location.pathname);
   }
 
+  if (location.host.includes("github.io")) {
+    goTo("/desafio-m5");
+  }
+
   //ESTO LO BORRE AL PRINCIPIO PERO PUEDE QUEDAR LUEGO
   /* handleRoute(location.pathname) */
 
-  window.onpopstate = ()=>{
-    handleRoute(location.pathname)
-  }
+  window.onpopstate = () => {
+    handleRoute(location.pathname);
+  };
 }
